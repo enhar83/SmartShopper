eaca803257e6b187e123a262b1962b1de90dec94
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-20 15:14:20 +0300

    Core/IRepositories içerisinde IUoW kodları yazıldı, DAL/Repositories'de UoW içerisinde IUoW'de yazılan metotların içerisi dolduruldu.

---
+++ Core Layer/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.IRepositories
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        Task SaveAsync();
+        void Save();
+    }
+}
---
+++ Data Access Layer/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Core_Layer.IRepositories;
+using Data_Access_Layer.DbContext;
+
+namespace Data_Access_Layer.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Save() => _context.SaveChanges();
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+
+    }
+}
