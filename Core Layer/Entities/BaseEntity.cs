2ec6531d1a7b5d1d1d561c07317c5a925e6382cb
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-20 12:58:22 +0300

    Core katmanı içerisine Entities klasörü açıldı, içerisine IEntity interface'i ve BaseEntity sınıfı eklendi.

---
+++ Core Layer/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Entities
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
---
+++ Core Layer/Entities/IEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Entities
+{
+    public interface IEntity
+    {
+    }
+}
+
+/*
+ 
+    IEntity Neden Boş?
+        - bir sınıfa etiket yapıştırır.
+        - IEntity şunu der: ben bir db tablosuyum.
+
+    Neden BaseEntity Miras Alıyor?
+        - BaseEntity ortak özellikleri taşırken, IEntity sınıfın kimliğini belirler.
+        - eğer BaseEntity, IEntityden miras alırsa; BaseEntityden türeyen tüm sınıflar otomatik olarak IEntity etiketiyle damgalanmış olur.
+
+    GenericConstraits
+        - neden boş bir arayüze ihtiyacımız olduğunun en net cevabı, ileride yazılacak repo katmanındadır.
+        - bir generic repo yazarken her türden nesnenin oraya girmesi istenilmez. 
+            * where T: class yazıldığı zaman string bir ifade bile oraya girebilir.      
+            * where T: class, IEntity, new() olarak yazıldığı taktirde ise sadece IEntity etiketine sahip olan sınıflar buraya girebilir.
+ 
+ */
