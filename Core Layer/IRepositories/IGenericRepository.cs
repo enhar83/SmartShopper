94c8ced29c81d4e988f538ac41721db7091c84ad
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-20 14:53:09 +0300

    Core katmanı içerisinde IRepositories klasörü içerisinde IGenericRepository oluşturuldu ve metotlar tanımlandı.

---
+++ Core Layer/IRepositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core_Layer.Entities;
+
+namespace Core_Layer.IRepositories
+{
+    public interface IGenericRepository<T> where T: class, IEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        Task Remove(T entity);
+    }
+}
