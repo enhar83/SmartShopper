c03a8d61a486f466681ed7a91beb95090b3af97f
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-21 11:25:20 +0300

    Core/Exceptions içerisine LogicException açıldı ve hata yönetimi özelleştirildi. Core/IServices içerisine ICategoryService ve Business/Managers içerisine CategoryManager açıldı. AddCategory işlemi için gereken kodlar yazıldı.

---
+++ Business Layer/Managers/CategoryManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos;
+using Core_Layer.Exceptions;
+using Core_Layer.IRepositories;
+using Core_Layer.IServices;
+using Entity_Layer;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Business_Layer.Managers
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public CategoryManager(ICategoryRepository categoryRepository,IUnitOfWork uow, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task TAddCategory(AddCategoryDto addCategoryDto)
+        {
+            var isExist = await _categoryRepository.AnyAsync(x => x.Name == addCategoryDto.Name);
+            if (isExist)
+                throw new LogicException(nameof(addCategoryDto.Name),"This category name is already exists.");
+
+            var category = _mapper.Map<Category>(addCategoryDto);
+
+            await _categoryRepository.AddAsync(category);
+            await _uow.SaveAsync();
+        }
+    }
+}
