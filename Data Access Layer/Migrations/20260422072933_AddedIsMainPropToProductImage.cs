de89b263a1e346a8a741b037dd12a599beae690c
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-22 10:30:35 +0300

    ProductImage içerisine IsMain propu eklendi ve migrationa hazır hale getirildi.

---
+++ Data Access Layer/Migrations/20260422072933_AddedIsMainPropToProductImage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsMainPropToProductImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMain",
+                table: "ProductImages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMain",
+                table: "ProductImages");
+        }
+    }
+}
--- Entity Layer/ProductImage.cs
+++ Entity Layer/ProductImage.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entity_Layer.Common;
+using Microsoft.AspNetCore.Http;
 
 namespace Entity_Layer
 {
     public class ProductImage: BaseEntity
     {
         public required string ImageUrl { get; set; }
+        public bool IsMain { get; set; }
         public Guid ProductId { get; set; }
-        public required Product Product { get; set; }
+        public Product? Product { get; set; }
     }
 }
