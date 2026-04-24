b06feb651207c50febd17d01c3595cea1c9670cf
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-24 17:45:38 +0300

    UI/Areas/Admin içerisinde UserController oluşturuldu, viewi yazıldı. Kullanıcı listesi tamamlandı.

---
+++ SmartShopper/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public UserController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var users = await _authService.TGetUserListAsync();
+            return View(users);
+        }
+    }
+}
