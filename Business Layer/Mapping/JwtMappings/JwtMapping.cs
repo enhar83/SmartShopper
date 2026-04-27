12f5e9773c5d023261ad0a3f170f4229193f4a44
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-27 20:42:29 +0300

    JwtDto oluşturuldu ve mapping kodları yazıldı.

---
+++ Business Layer/Mapping/JwtMappings/JwtMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.JwtDtos;
+using Entity_Layer;
+
+namespace Business_Layer.Mapping.JwtMappings
+{
+    public class JwtMapping:Profile
+    {
+        public JwtMapping()
+        {
+            CreateMap<AppUser, JwtDto>()
+                .ForMember(dest => dest.Token, opt => opt.Ignore());
+        }       
+    }
+}
---
+++ Core Layer/Dtos/JwtDtos/JwtDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Dtos.JwtDtos
+{
+    public class JwtDto
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required List<string> Roles { get; set; }
+        public required string Token { get; set; }
+    }
+}
