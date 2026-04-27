8bd52c9cd149e3c80e52fa7b8bc7e993830b7cb0
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-27 13:58:54 +0300

    CreateRole işlemi için service ve manager kodları tamamlandı.

---
+++ Business Layer/Managers/RoleManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.RoleDtos;
+using Core_Layer.Exceptions;
+using Core_Layer.IServices;
+using Entity_Layer;
+using Microsoft.AspNetCore.Identity;
+
+namespace Business_Layer.Managers
+{
+    public class RoleManager: IRoleService
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RoleManager(RoleManager<AppRole> roleManager, IMapper mapper)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IdentityResult> TCreateRoleAsync(CreateRoleDto createRoleDto)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(createRoleDto.RoleName);
+
+            if (roleExist)
+                throw new LogicException("RoleName", "This role already exists in the system.");
+
+            var role = _mapper.Map<AppRole>(createRoleDto);
+
+            var result = await _roleManager.CreateAsync(role);
+            return result;
+        }
+    }
+}
---
+++ Core Layer/IServices/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core_Layer.Dtos.RoleDtos;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core_Layer.IServices
+{
+    public interface IRoleService
+    {
+        Task<IdentityResult> TCreateRoleAsync(CreateRoleDto createRoleDto);
+    }
+}
