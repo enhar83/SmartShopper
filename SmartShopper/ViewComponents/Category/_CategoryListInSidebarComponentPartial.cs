eaf0c8e07ca0c5e4ca63c18c020d93a5d3992681
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-29 16:25:54 +0300

    CategoryListInSidebar için viewcomponent kodları yazıldı ve view oluşturuldu.

---
+++ SmartShopper/ViewComponents/Category/_CategoryListInSidebarComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using Core_Layer.IRepositories;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Category
+{
+    public class _CategoryListInSidebarComponentPartial:ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public _CategoryListInSidebarComponentPartial(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _categoryService.TGetCategoriesForSidebarAsync();
+            return View(categories);
+        }
+    }
+}
