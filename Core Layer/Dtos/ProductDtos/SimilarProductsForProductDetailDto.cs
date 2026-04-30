0803d48cb59e1ecf1e051128d51beab636b385e9
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-30 14:23:26 +0300

    SimilarProductsForProductDetailDto oluşturuldu ve mapping kodları tamamlandı.

---
+++ Core Layer/Dtos/ProductDtos/SimilarProductsForProductDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Dtos.ProductDtos
+{
+    public class SimilarProductsForProductDetailDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string CategoryName { get; set; }
+        public required string SubCategoryName { get; set; }
+        public double Price { get; set; }
+        public string? MainImageUrl { get; set; }
+        public required string StockStatus { get; set; }
+        public required string StockStatusClass { get; set; }
+    }
+}
