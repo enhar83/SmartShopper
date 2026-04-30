0cc39016139f641d2d911c9ac474df65ef54d256
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-04-30 16:19:03 +0300

    Layout içerisinde head alanı view componente ayrıldı, navbar alanında ise kendi içerisinde ayrılmaya başlandı.

---
+++ SmartShopper/ViewComponents/Layout/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Layout
+{
+    public class _LayoutNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartShopper/ViewComponents/Layout/_LayoutNavbarViewComponents/_LayoutMainMenuComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Layout._LayoutNavbarViewComponents
+{
+    public class _LayoutMainMenuComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
