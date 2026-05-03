4c686f4c183937bd048a50f8f0397277fa2d5ec8
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-03 15:36:08 +0300

    UserAddressController içerisinde UserAddressList action'ı yazıldı, ViewProfile view'i içerisinde modal kullanılarak kullanıcı adresleri listelendi.

---
+++ SmartShopper/Controllers/UserAddressController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Bogus.DataSets;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.Controllers
+{
+    public class UserAddressController : Controller
+    {
+        private readonly IUserAddressService _userAddressService;
+
+        public UserAddressController(IUserAddressService userAddressService)
+        {
+            _userAddressService = userAddressService;
+        }
+
+        public async Task<IActionResult> UserAddressList()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userIdString == null)
+                return Unauthorized();
+
+            var userId = Guid.Parse(userIdString);
+
+            var userAddresses = await _userAddressService.TGetUserAddressListAsync(userId);
+            return Json(new { succeeded = true, data = userAddresses });
+        }
+    }
+}
