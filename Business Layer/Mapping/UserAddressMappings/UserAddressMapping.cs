44b6e0f63aaf2bc526710514a810e5b8fdf1e9d2
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-03 15:19:08 +0300

    UserAddressListDto oluşturuldu ve mapping kodları tamamlandı.

---
+++ Business Layer/Mapping/UserAddressMappings/UserAddressMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.UserAddressDtos;
+using Entity_Layer;
+
+namespace Business_Layer.Mapping.UserAddressMappings
+{
+    public class UserAddressMapping: Profile
+    {
+        public UserAddressMapping() 
+        {
+            CreateMap<UserAddress, UserAddressListDto>().ReverseMap();
+        }
+    }
+}
