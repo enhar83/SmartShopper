d1b5cc273e993022987b92ce213c51a8c65c34ce
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-03 14:14:31 +0300

    ProfileController oluşturuldu ve ViewProfile actionu yazıldı. View tarafı da tamamlandı.

---
+++ SmartShopper/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        public async Task<IActionResult> ViewProfile()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userIdString == null)
+                return RedirectToAction("Login","Account");
+
+            var userId = Guid.Parse(userIdString);
+
+            var userProfile = await _profileService.TGetProfileAsync(userId);
+            return View(userProfile);
+        }
+    }
+}
