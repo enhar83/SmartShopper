a39cec7d0d7268eba937d97fd58ca6fdec121fac
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-03 14:36:40 +0300

    ProfileManager içerisinde TEditProfile metodu tanımlandı. FileStream işlemleri ve uniqueness kontrolleri manager içerisinde yapıldı.

--- Business Layer/Managers/ProfileManager.cs
+++ Business Layer/Managers/ProfileManager.cs
@@ -9,10 +9,12 @@ using Core_Layer.Exceptions;
 using Core_Layer.IServices;
 using Entity_Layer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MimeKit.Encodings;
 
 namespace Business_Layer.Managers
 {
-    public class ProfileManager:IProfileService
+    public class ProfileManager : IProfileService
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly IMapper _mapper;
@@ -21,6 +23,55 @@ namespace Business_Layer.Managers
         {
             _userManager = userManager;
             _mapper = mapper;
+        }
+
+        public async Task<IdentityResult> TEditProfileAsync(EditProfileDto editProfileDto)
+        {
+            var user = await _userManager.FindByIdAsync(editProfileDto.Id.ToString());
+            if (user == null)
+                throw new LogicException("Id", "User record could not be found.");
+
+            if (user.UserName != editProfileDto.UserName && await _userManager.Users.AnyAsync(u => u.UserName == editProfileDto.UserName))
+                throw new LogicException("UserName", "This username is already taken.");
+
+            if (user.PhoneNumber != editProfileDto.PhoneNumber && await _userManager.Users.AnyAsync(u => u.PhoneNumber == editProfileDto.PhoneNumber))
+                throw new LogicException("PhoneNumber", "This phone number is already registered.");
+
+            if (editProfileDto.UserImage != null && editProfileDto.UserImage.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(editProfileDto.UserImage.FileName);
+                var imageName = $"{Guid.NewGuid()}{extension}";
+
+                var relativePath = $"images/users/{imageName}";
+                var absolutePath = Path.Combine(rootPath, "wwwroot", "images", "users", imageName);
+
+                var directoryPath = Path.GetDirectoryName(absolutePath);
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath!);
+
+                if (!string.IsNullOrEmpty(user.ImageUrl))
+                {
+                    var oldFilePath = Path.Combine(rootPath, "wwwroot", user.ImageUrl.TrimStart('/'));
+                    if (File.Exists(oldFilePath))
+                        File.Delete(oldFilePath);
+                }
+
+                using (var stream = new FileStream(absolutePath, FileMode.Create))
+                {
+                    await editProfileDto.UserImage.CopyToAsync(stream);
+                }
+
+                user.ImageUrl = "/" + relativePath;
+            }
+
+            _mapper.Map(editProfileDto, user);
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+                await _userManager.UpdateSecurityStampAsync(user);
+
+            return result;
         }
 
         public async Task<ViewProfileDto> TGetProfileAsync(Guid id)
--- Core Layer/IServices/IProfileService.cs
+++ Core Layer/IServices/IProfileService.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Core_Layer.Dtos.ProfileDtos;
+using Microsoft.AspNetCore.Identity;
 
 namespace Core_Layer.IServices
 {
     public interface IProfileService
     {
         Task<ViewProfileDto> TGetProfileAsync(Guid id);
+        Task<IdentityResult> TEditProfileAsync(EditProfileDto editProfileDto);
     }
 }
