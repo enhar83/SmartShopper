37a0bdbb5891d967bb8242576811862e73871121
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-03 13:48:51 +0300

    ViewProfileDto oluşturuldu ve mapping kodları yazıldı.

---
+++ Business Layer/Mapping/ProfileMappings/ProfileMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.ProfileDtos;
+using Entity_Layer;
+
+namespace Business_Layer.Mapping.ProfileMappings
+{
+    public class ProfileMapping:Profile
+    {
+        public ProfileMapping() 
+        {
+            CreateMap<ViewProfileDto, AppUser>().ReverseMap();
+        }   
+    }
+}
---
+++ Core Layer/Dtos/ProfileDtos/ViewProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Dtos.ProfileDtos
+{
+    public class ViewProfileDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public string? City { get; set; }
+        public string? ImageUrl { get; set; }
+        public required string UserName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public required string Email { get; set; }
+    }
+}
