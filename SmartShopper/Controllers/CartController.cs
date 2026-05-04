3a00928db247210eca6fbf6c8bc6419d2d822603
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-04 13:52:38 +0300

    CartController oluşturuldu, Index viewi oluşturuldu ve getcartdata ile sepet verileri gösterildi.

---
+++ SmartShopper/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetCartData()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userIdString == null) 
+                return RedirectToAction("Login", "Account");
+
+            var userId = Guid.Parse(userIdString);
+            var cartDto = await _cartService.TGetUserCartAsync(userId);
+
+            return Json(cartDto);
+        }
+    }
+}
