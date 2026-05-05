49d9489b88a5c1af18f0c19acf2176c700f36cc4
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-05 11:41:24 +0300

    Navbar içerisinde Mini Cart gösterimi için _CartNavbar View componenti ve view'i oluşturuldu.

---
+++ SmartShopper/ViewComponents/Cart/_CartNavbarComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Core_Layer.Dtos.CartDtos;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Cart
+{
+    public class _CartNavbarComponentPartial:ViewComponent
+    {
+        private readonly ICartService _cartService;
+        public _CartNavbarComponentPartial(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userIdClaim = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userIdClaim))
+                return View(new CartDto { Items = new List<CartItemDto>() });
+
+            var userId = Guid.Parse(userIdClaim);
+            var cart = await _cartService.TGetUserCartAsync(userId);
+
+            return View(cart);
+        }
+    }
+}
