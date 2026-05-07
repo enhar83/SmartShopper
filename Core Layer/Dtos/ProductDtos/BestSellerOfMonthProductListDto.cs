1abd5d4a2e0bd7c013e0381cf7e500e72e5ed069
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-07 11:33:30 +0300

    BestSellerOfMonthProductListDto oluşturuldu ve mapping kodları tamamlandı.

---
+++ Core Layer/Dtos/ProductDtos/BestSellerOfMonthProductListDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Dtos.ProductDtos
+{
+    public class BestSellerOfMonthProductListDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string MainImageUrl { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+        public string SubCategoryName { get; set; } = null!;
+        public int MonthlySalesCount { get; set; } 
+        public bool IsFavorite { get; set; }
+    }
+}
