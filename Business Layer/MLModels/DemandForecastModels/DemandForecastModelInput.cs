503f5fe88c8eb4c1f59c9b26575ee299b7e65231
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-11 14:35:11 +0300

    DemandForecast için MLModels sınıfları yazıldı, Dto oluşturuldu ve mapping kodları tamamlandı.

---
+++ Business Layer/MLModels/DemandForecastModels/DemandForecastModelInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace Business_Layer.MLModels.DemandForecastModels
+{
+    public class DemandForecastModelInput
+    {
+        [LoadColumn(0)]
+        public string Country { get; set; } = null!;
+        [LoadColumn(1)]
+        public string City { get; set; } = null!;
+        [LoadColumn(2)]
+        public float Month { get; set; }
+        [LoadColumn(3)]
+        public float Year { get; set; }
+        [LoadColumn(4)]
+        [ColumnName("Label")]
+        public float TotalRevenue { get; set; }
+    }
+}
---
+++ Business Layer/Mapping/DemandForecastMappings/DemandForecastMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.DemandForecastDtos;
+using Entity_Layer;
+
+namespace Business_Layer.Mapping.DemandForecastMappings
+{
+    public class DemandForecastMapping:Profile
+    {
+        public DemandForecastMapping()
+        {
+            CreateMap<RegionalDemandForecast, RegionalDemandForecastDto>();
+        }
+    }
+}
