b4003ad4a0ba35a44c447e342ee8a5a5b3024d42
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-11 10:06:31 +0300

    SalesForecastModelInput ve Output sınıfları oluşturuldu, SalesForecastResultDto oluşturuldu.

---
+++ Business Layer/MLModels/SalesForecastingModels/SalesForecastingModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.MLModels.SalesForecastingModels
+{
+    public class SalesForecastingModelInput
+    {
+        public float MonthIndex { get; set; }
+        public float TotalSales { get; set; }
+    }
+}
---
+++ Core Layer/Dtos/SalesForecastDtos/SalesForecastResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.Dtos.SalesForecastDtos
+{
+    public class SalesForecastResultDto
+    {
+        public string Period { get; set; } = null!; 
+        public double ActualAmount { get; set; }
+        public double ForecastAmount { get; set; }
+        public bool IsForecast { get; set; }
+    }
+}
