6d3ea46829ae74ddccbca7f60c89b1fb17c3ef49
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-18 15:28:07 +0300

    ProductDetail sayfası içerisinde ürüne yorum ekleme tamamlandı, viewcomponent, controller ve view düzenlendi.

---
+++ SmartShopper/ViewComponents/Comment/_CommentListForProductDetailComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Comment
+{
+    public class _CommentListForProductDetailComponentPartial:ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public _CommentListForProductDetailComponentPartial(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid productId)
+        {
+            var comments = await _commentService.TGetApprovedCommentsByProductIdAsync(productId);
+            return View(comments);
+        }
+    }
+}
