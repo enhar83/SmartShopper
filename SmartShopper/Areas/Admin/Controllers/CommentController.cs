eab98b282ac85216739c40df92b81aa42b98c1fa
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-18 16:53:51 +0300

    CommentList tamamlandı, comment status değiştirme admin panel içerisinde bitti.

---
+++ SmartShopper/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using Core_Layer.Exceptions;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task<IActionResult> CommentList()
+        {
+            var comments = await _commentService.TGetAllCommentsForAdminAsync();
+            return View(comments);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleApproval(Guid id)
+        {
+            try
+            {
+                await _commentService.TToggleCommentApprovalAsync(id);
+
+                return Json(new { success = true, message = "Comment visibility status has been successfully updated." });
+            }
+            catch (LogicException ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "A technical error occurred while updating the comment status." });
+            }
+        }
+    }
+}
