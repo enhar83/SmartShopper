bbdd6938d0e1844a7964ace07d53b74808e5c2bd
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-20 15:22:52 +0300

    CreateNotificationDto oluşturuldu ve mapping kodları yazıldı.

---
+++ Business Layer/Mapping/NotificationMappings/NotificationMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core_Layer.Dtos.NotificationDtos;
+using Entity_Layer;
+
+namespace Business_Layer.Mapping.NotificationMappings
+{
+    public class NotificationMapping:Profile
+    {
+        public NotificationMapping()
+        {
+            CreateMap<CreateNotificationDto, Notification>().ReverseMap();
+        }
+    }
+}
