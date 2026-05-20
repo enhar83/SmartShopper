5aa5bcccdfbc2fbc903311b3ddf8cc72f6e66fd9
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-05-20 16:09:26 +0300

    Notification için viewcomponent oluşturuldu, notificationlist navbar üzerinde gösterildi.

---
+++ SmartShopper/ViewComponents/Notification/_NavbarNotificationListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Core_Layer.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShopper.ViewComponents.Notification
+{
+    public class _NavbarNotificationListComponentPartial : ViewComponent
+    {
+        private readonly INotificationService _notificationService;
+        public _NavbarNotificationListComponentPartial(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userIdString = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userIdString))
+                return View(); 
+
+            Guid userId = Guid.Parse(userIdString);
+
+            ViewBag.UnreadCount = await _notificationService.TGetUnreadNotificationCountAsync(userId);
+            var notifications = await _notificationService.TGetUserNotificationsAsync(userId);
+
+            return View("Default", notifications);
+        }
+    }
+}
